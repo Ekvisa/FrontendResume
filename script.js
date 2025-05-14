75fc57ba1a8f3ca5fa163072cedc0237f7c85d83
Ekvisa <katishh@gmail.com>
2025-05-14 15:29:27 +0400

    Hover behaviour added

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const paths = document.querySelectorAll("svg path");
 const tooltips = document.querySelectorAll(".tooltip");
 const jobs = document.querySelectorAll("#experience_block b");
-console.log(tooltips);
-console.log(jobs);
 
 paths.forEach((path) => {
   let isTooltipVisible = false;
@@ -24,8 +22,8 @@ paths.forEach((path) => {
   });
 });
 
-//Hide the tooltip only if cursor is not on the path:
 tooltips.forEach((tooltip) => {
+  //Hide the tooltip only if cursor is not on the path:
   tooltip.addEventListener("mouseleave", () => {
     const path = document.getElementById(tooltip.id.replace("_tip", ""));
     if (!path.matches(":hover")) {
@@ -33,46 +31,50 @@ tooltips.forEach((tooltip) => {
       isTooltipVisible = false;
     }
   });
+  //Highlight according opened blocks:
+  tooltip.addEventListener("mouseenter", () => {
+    const blockClass = tooltip.id.replace("_tip", "-block");
+    const shinyBlocks = document.getElementsByClassName(blockClass);
+    console.log(shinyBlocks);
+    Array.from(shinyBlocks).forEach((block) => {
+      block.classList.add("shine");
+    });
+  });
+  tooltip.addEventListener("mouseleave", () => {
+    const blockClass = tooltip.id.replace("_tip", "-block");
+    const shinyBlocks = document.getElementsByClassName(blockClass);
+    Array.from(shinyBlocks).forEach((block) => {
+      block.classList.remove("shine");
+    });
+  });
 });
 
 //Show the resume block when click on the tooltip:
 function openBlockByClick(element) {
   element.addEventListener("click", () => {
+    if (
+      !document.getElementById("comment-block").classList.contains("hidden")
+    ) {
+      document.getElementById("comment-block").classList.add("hidden");
+    }
     const targetId = element.id.replace("_tip", "_block");
-    console.log(targetId);
     const resumeBlock = document.getElementById(targetId);
     if (resumeBlock) {
       if (resumeBlock.classList.contains("hidden")) {
         resumeBlock.classList.remove("hidden");
-      } else resumeBlock.classList.add("hidden");
+      }
     }
+    resumeBlock.scrollIntoView();
   });
 }
 
 tooltips.forEach((tooltip) => {
   openBlockByClick(tooltip);
-  if (!document.getElementById("comment-block").classList.contains("hidden")) {
-    document.getElementById("comment-block").classList.add("hidden");
-  }
 });
 
 jobs.forEach((job) => {
-  console.log(job);
   openBlockByClick(job);
 });
-
-// tooltips.forEach((tooltip) => {
-//   tooltip.addEventListener("click", () => {
-//     const targetId = tooltip.id.replace("_tip", "_block");
-//     const resumeBlock = document.getElementById(targetId);
-//     if (resumeBlock) {
-//       resumeBlock.classList.remove("hidden");
-//     }
-//     if (!greeting.classList.contains("hidden")) {
-//       greeting.classList.add("hidden");
-//     }
-//   });
-// });
 
 let isAllShown = false;
 
@@ -83,7 +85,6 @@ const resumeBlocks = document.querySelectorAll(".resume-block");
 function showAllBlocks() {
   if (showAllButton.innerText === "Раскрыть всё") {
     resumeBlocks.forEach((block) => {
-      console.log(block);
       block.classList.remove("hidden");
     });
     if (
@@ -95,7 +96,6 @@ function showAllBlocks() {
     showAllButton.innerText = "Спрятать всё";
   } else {
     resumeBlocks.forEach((block) => {
-      console.log("-");
       block.classList.add("hidden");
     });
     document.getElementById("comment-block").classList.remove("hidden");
@@ -106,6 +106,7 @@ function showAllBlocks() {
 showAllButton.addEventListener("click", showAllBlocks);
 
 resumeBlocks.forEach((block) => {
+  //Add Close buttons:
   const closeBtn = document.createElement("button");
   closeBtn.textContent = "×";
   closeBtn.classList.add("close-btn");
@@ -113,11 +114,33 @@ resumeBlocks.forEach((block) => {
     block.classList.add("hidden");
   });
   block.appendChild(closeBtn);
+
+  //Highlight according tip:
+  function findTipId(el) {
+    const blockClasses = el.className.split(" ");
+    for (let blockClass of blockClasses) {
+      const tipId = blockClass.replace("-block", "_tip");
+      const tip = document.getElementById(tipId);
+      if (tip) {
+        return tip;
+      }
+    }
+  }
+  block.addEventListener("mouseenter", () => {
+    block.classList.add("shine");
+    const tipToShow = findTipId(block);
+    tipToShow.classList.remove("hidden");
+  });
+  block.addEventListener("mouseleave", () => {
+    block.classList.remove("shine");
+    const tipToShow = findTipId(block);
+    tipToShow.classList.add("hidden");
+  });
 });
 
+//Scroll to top:
 const scrollTopBtn = document.getElementById("scrollTopBtn");
 const girl = document.querySelector(".img-wrapper");
-
 window.addEventListener("scroll", () => {
   const girlRect = girl.getBoundingClientRect(); //rectangle with girl in it
   if (girlRect.top < 0) {
@@ -126,7 +149,6 @@ window.addEventListener("scroll", () => {
     scrollTopBtn.classList.add("hidden");
   }
 });
-
 scrollTopBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0 });
 });
